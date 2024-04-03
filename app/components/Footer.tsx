235fccbc1e92c5fcf4caaeb38fe9a1c8e8d42187
Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-04-03 08:29:50 +0200

    finished footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,35 @@ import '../globals.css';
 export default function Footer() {
 	return (
 		<footer>
-			<h1>awe</h1>
+			<div className='footer-body'>
+				<div className='footer-left'>
+					<p>Want to discuss a project or have a job offer?</p>
+					<a href='mailto:'>Get in touch</a>
+				</div>
+				<div className='footer-right'>
+					<div>
+						<p>Navigate</p>
+						<a href='http://' target='_blank' rel='noopener noreferrer'>
+							Work
+						</a>
+						<a href='http://' target='_blank' rel='noopener noreferrer'>
+							Photography
+						</a>
+					</div>
+					<div>
+						<p>Connect</p>
+						<a href='http://' target='_blank' rel='noopener noreferrer'>
+							LinkedIn
+						</a>
+						<a href='http://' target='_blank' rel='noopener noreferrer'>
+							Instagram
+						</a>
+						<a href='http://' target='_blank' rel='noopener noreferrer'>
+							Dribble
+						</a>
+					</div>
+				</div>
+			</div>
 		</footer>
 	);
 }

429b3f5e522f6d5999ed7f3d45dcef8c3168608e
Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-05-08 20:09:15 +0200

    finished anigram and images

---
+++ app/anigram/page.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import React from 'react';
+import './anigram.css';
+
+export default function page() {
+	return (
+		<section className='anigram'>
+			<div className='max-width'>
+				<div className='anigram-head'>
+					<h1>WHOOPS... NOTHING MORE TO SEE HERE</h1>
+					<p>
+						Excitement mounts as I prepare to unveil the latest creation. Stay
+						tuned to witness the unfolding of what Anigram will be.
+					</p>
+				</div>
+				<div className='anigram-body'>
+					<Image src={'/pet1.png'} width={500} height={800} alt='pet 1' />
+					<Image src={'/pet2.png'} width={500} height={800} alt='pet 2' />
+				</div>
+			</div>
+		</section>
+	);
+}
